3d3312447b23b16de8f64d39b66cb3cf1bcea1d4
Derenik Petrosyan <deren_petrosyan@mail.ru>
2021-07-12 11:47:45 +0400

    new method

--- src/com/iunetworks/com/iunetworks/ActionInteger.java
+++ src/com/iunetworks/com/iunetworks/ActionInteger.java
@@ -1,51 +1,25 @@
 package com.iunetworks;
 
-import java.util.Arrays;
-
 public class ActionInteger {
-    public static void sumInteger(char[] X, char[] Y) {
-        char[] Z;
-        if (X.length < Y.length) {
-            Z = new char[Y.length + 1];
-            for (int i = 0; i < Z.length; i++) {
-                Z[i] = '0';
-            }
-            for (int i = 1; i < X.length + 1; i++) {
-                Z[Z.length - i] = (char) (Z[Z.length - i] + (char) (X[X.length - i] + Y[Y.length - i] - 2*'0'));
-                if (Z[Z.length - i] > '9') {
-                    Z[Z.length - i] = (char) (Z[Z.length - i] - 10);
-                    Z[Z.length - i - 1] = (char) ((char) Z[Z.length - i - 1] + 1);
-                }
-            }
-            for (int i = X.length + 1; i < Y.length + 1; i++) {
-                Z[Z.length - i] = (char) (Z[Z.length - i] + Y[Y.length - i] - '0');
-                if (Z[Z.length - i] > '9') {
-                    Z[Z.length - i] = (char) (Z[Z.length - i] - 10);
-                    Z[Z.length - i - 1] = (char) ((char) Z[Z.length - i - 1] + 1);
-                }
-            }
-        } else {
-            Z = new char[X.length + 1];
-            for (int i = 0; i < Z.length; i++) {
-                Z[i] = '0';
-            }
-            for (int i = 1; i < Y.length + 1; i++) {
-                Z[Z.length - i] = (char) (Z[Z.length - i] + (char) (Y[Y.length - i] + X[X.length - i] - 96));
-                if (Z[Z.length - i] > 57) {
-                    Z[Z.length - i] = (char) (Z[Z.length - i] - 10);
-                    Z[Z.length - i - 1] = (char) ((char) Z[Z.length - i - 1] + 1);
-                }
-            }
-            for (int i = Y.length + 1; i < X.length + 1; i++) {
-                Z[Z.length - i] = (char) (Z[Z.length - i] + X[X.length - i] - 48);
-                if (Z[Z.length - i] > 57) {
-                    Z[Z.length - i] = (char) (Z[Z.length - i] - 10);
-                    Z[Z.length - i - 1] = (char) ((char) Z[Z.length - i - 1] + 1);
-                }
-            }
+    public static char[] reversCHarArray(char[] crowd) {
+        for (int i = 0; i < crowd.length / 2; i++) {
+            char box = crowd[i];
+            crowd[i] = crowd[crowd.length - 1 - i];
+            crowd[crowd.length - 1 - i] = box;
         }
+        return crowd;
+    }
 
-        System.out.println(Arrays.toString(Z));
+    public static void digitInteger(int number) {
+        int wholeNumber = number;
+        int balanceNumber = 0;
+        do {
+            balanceNumber = wholeNumber % 10;
+            wholeNumber = wholeNumber / 10;
+            System.out.println(balanceNumber);
+        }
+        while (wholeNumber > 0);
+
     }
 }
 
